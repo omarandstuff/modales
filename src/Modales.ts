0e0bf60a28d047b3c4423b389eae60bb5a2ed671
David De Anda <omarandstuff@gmail.com>
2019-07-03 23:37:20 -0500

    Main interface (working on it)

---
+++ src/Modales.ts
@@ -0,0 +1,7 @@
+import ProviderHelper from './ProviderHelper'
+import { Router } from './Modales.types'
+
+export default class Modales {
+  public providerHelper: ProviderHelper = new ProviderHelper()
+  public router: Router = null
+}
