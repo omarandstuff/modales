3c8b3718106ce8934c6decb24710374d6709f9fb
David De Anda <omarandstuff@gmail.com>
2019-07-04 13:16:23 -0500

    Update types

--- src/Modales.types.ts
+++ src/Modales.types.ts
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Location, History } from 'history'
+import { Location } from 'history'
 
 export type ModalBackground = 'transparent' | 'translucent' | 'blured'
 
@@ -13,43 +13,4 @@ export interface Modal {
   onScape?: (event: KeyboardEvent) => void
   type: 'route' | 'custom'
   withOutInitialAnimation: boolean
-}
-
-export interface Router {
-  block(pathname: string, state: {}): void
-  isModalLocation(location: Location): boolean
-  go(pathname: string, state: {}): void
-  goBack(pathname: string, state: {}): void
-  goForward(pathname: string, state: {}): void
-  push(pathname: string, state: {}): void
-  replace(pathname: string, state: {}): void
-}
-
-export interface ModalesInstance {
-  providerHelper: ProviderHelperInstance
-  router: Router
-}
-
-export interface ProviderHelperInstance {
-  blurEnabled: boolean
-  providerCallBack: (modals: Modal[]) => void
-  routeModalsEnabled: boolean
-  clearModals(modalId?: number, inclusive?: boolean): void
-  isModalLocation(location: Location): boolean
-  forceClearModals(modalId?: number): void
-  launchModal(
-    component: React.ReactNode | React.ReactNode[],
-    onOutsideClick: (event: MouseEvent) => boolean,
-    onScape: (event: KeyboardEvent) => boolean,
-    background?: string
-  ): Modal
-  launchRouteModal(
-    location: Location,
-    routes: React.ReactNode | React.ReactNode[],
-    onOutsideClick: (event: MouseEvent) => void,
-    onScape: (event: KeyboardEvent) => void,
-    withOutInitialAnimation: boolean
-  ): Modal
-  popModal(modalId?: number): void
-  setRouterBridge(newLocation: Location, historyRef: History): void
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default } from './Modales'
 export { default as ModalesProvider } from './ModalesProvider'
-export { ModalesInstance, Modal, ModalBackground } from './Modales.types'
+export { Modal, ModalBackground } from './Modales.types'
