cad850a14586de3d9d3e72a7866dcf51fabe9637
David De Anda <omarandstuff@gmail.com>
2019-07-07 00:42:27 -0500

    Configure tests and ts tests

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest'
   },
   testRegex: '(/test/.*\\.test\\.tsx?)$',
-  collectCoverageFrom: ['src/**/*.ts']
+  collectCoverageFrom: ['src/**/*.ts'],
+  setupFilesAfterEnv: ['./node_modules/jest-enzyme/lib/index.js', '<rootDir>/test/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/test/__mocks__/styleMock.ts'
+  }
 }
