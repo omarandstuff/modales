b215c227d411c86e6cf07cc459a1d29e99d25ee0
David De Anda <omarandstuff@gmail.com>
2019-07-09 16:27:18 -0500

    Use helpers

--- test/ModalesProvider.test.ts
+++ test/ModalesProvider.test.ts
@@ -1,82 +1,15 @@
 import * as React from 'react'
 import { shallow, ShallowWrapper } from 'enzyme'
-import { Location, History, Action } from 'history'
+import { Location, History } from 'history'
 import { match } from 'react-router'
 
 import Modales from '../src'
-import ProviderHelper from '../src/ProviderHelper'
-import { ModalesProvider } from '../src/ModalesProvider'
+import ModalesProviderX, { ProviderTestInterface } from './helpers/ModalesProviderX'
+import generateLocation from './helpers/generateLocation'
+import generateHistory from './helpers/generateHistory'
+import generateMatch from './helpers/generateMatch'
 
 jest.useFakeTimers()
-
-function generateLocation(pathname?: string, key?: string, state?: {}): Location {
-  return {
-    key: key,
-    pathname: pathname || '',
-    state: state || {},
-    search: '',
-    hash: ''
-  }
-}
-
-function generateHistory(location?: Location, action?: Action): History {
-  return {
-    length: 0,
-    location: location || generateLocation(),
-    action: action || 'PUSH',
-    push: jest.fn(),
-    replace: jest.fn(),
-    go: jest.fn(),
-    goBack: jest.fn(),
-    goForward: jest.fn(),
-    block: jest.fn(),
-    listen: jest.fn(),
-    createHref: jest.fn()
-  }
-}
-
-function generateMatch(params?: {}, path?: string, isExact?: boolean, url?: string): match {
-  return {
-    params: params || {},
-    path: path || '',
-    isExact: isExact || false,
-    url: url || ''
-  }
-}
-
-interface ProviderTestInterface {
-  baseLocation?: Location
-  intialId?: string
-  historyIndex?: number
-  historyOrder?: string[]
-  historyMap?: { [key: string]: Location }
-  lastLocation?: Location
-  providerHelper?: ProviderHelper
-}
-
-class ModalesProviderX extends ModalesProvider {
-  public setTestInterface(testInterface: ProviderTestInterface): void {
-    this.baseLocation = testInterface.baseLocation || this.baseLocation
-    this.intialId = testInterface.intialId || this.intialId
-    this.historyIndex = testInterface.historyIndex || this.historyIndex
-    this.historyOrder = testInterface.historyOrder || this.historyOrder
-    this.historyMap = testInterface.historyMap || this.historyMap
-    this.lastLocation = testInterface.lastLocation || this.lastLocation
-    this.providerHelper = testInterface.providerHelper || this.providerHelper
-  }
-
-  public getTestInterface(): ProviderTestInterface {
-    return {
-      baseLocation: this.baseLocation,
-      intialId: this.intialId,
-      historyIndex: this.historyIndex,
-      historyOrder: this.historyOrder,
-      historyMap: this.historyMap,
-      lastLocation: this.lastLocation,
-      providerHelper: this.providerHelper
-    }
-  }
-}
 
 beforeEach((): void => {})
 
